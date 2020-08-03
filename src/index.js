4078571d46deabd3893045a926df2cfe4bdfbce9
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-08-03 16:50:18 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//allows jquery to be used
 const $ = require('jquery');
 
 import sayHello from "./say-hello.js"
--- src/say-hello.js
+++ src/say-hello.js
@@ -1,4 +1,4 @@
-
+//function that is going to be exported to another file
 const sayHello = (name = "hello") => console.log(name);
 
 module.exports = sayHello;
